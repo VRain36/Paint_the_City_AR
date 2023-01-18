969dc430425adaabb6ce92c717378c9feed94d73
dudskrla <dudskrla09@gmail.com>
2023-01-18 15:11:35 +0900

    [Feat] 레포지토리 변경
    - Paint_the_City_AR_FE 레포지토리 -> Paint_the_City_AR 레포지토리로 변경

---
+++ Assets/Scripts/ARManager.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+
+using System.Collections.Generic;
+
+using UnityEngine;
+
+using System;
+
+using Vuforia;
+
+public class ARManager : MonoBehaviour
+
+{
+
+    public PlaneFinderBehaviour finder;
+
+    SmartTerrain smartTerrain;
+
+    PositionalDeviceTracker positionalDeviceTracker;
+
+    ContentPositioningBehaviour contentPositioningBehaviour;
+
+    AnchorBehaviour planeAnchor, midAirAnchor, placementAnchor;
+
+    StateManager stateManager;
+
+    float touchesPrePosDifference, touchesCurPosDifference, zoomModifier;
+
+    Vector2 Test, firstTouchPrevPos, secondTouchPrevPos;
+
+    public GameObject obj; // The 3d model that we want to show on the position
+
+    public bool ShowModelState = false;
+
+    public bool CanSupport = false;
+
+    private string consoleString; // Current value of message in console
+
+    public void LaunchModel()
+
+    {
+
+        finder.PerformHitTest(Test);
+
+        ShowModel();
+
+    }
+
+    public void ShowModel()
+
+    {
+
+        obj.SetActive(true);
+
+        ShowModelState = true;
+
+    }
+
+    public void HideModel()
+
+    {
+
+        obj.SetActive(false);
+
+        ShowModelState = false;
+
+    }
+
+    public void ResetTrackers()
+
+    {
+
+        this.smartTerrain = TrackerManager.Instance.GetTracker<SmartTerrain>();
+
+        this.positionalDeviceTracker = TrackerManager.Instance.GetTracker<PositionalDeviceTracker>();
+
+        this.smartTerrain.Stop();
+
+        this.positionalDeviceTracker.Reset();
+
+        this.smartTerrain.Start();
+
+    }
+
+    private void OnEnable()
+
+    {
+
+        Application.logMessageReceived += HandleLog;
+
+    }
+
+    private void OnDisable()
+
+    {
+
+        Application.logMessageReceived -= HandleLog;
+
+    }
+
+    void HandleLog(string message , string stacktrace, LogType type)   
+
+    {
+
+        consoleString = consoleString + "\n" + message;
+
+        if (consoleString.Contains("SmartTerrain failed"))
+
+        {
+
+            CanSupport = false;
+
+            print("No, This device can't support the surface tracking");
+
+        }
+
+        else
+
+        {
+
+            CanSupport = true;
+
+            print("Yes, This device can support the surface tracking");
+
+        }
+
+    }
+
+}
