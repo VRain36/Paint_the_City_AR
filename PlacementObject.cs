39e9a79f2b9c5c1af059df5c7f54e8292a394233
hyerinseo <106965575+hyerinseo@users.noreply.github.com>
2023-03-06 11:18:13 +0900

    Create PlacementObject.cs

---
+++ PlacementObject.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlacementObject : MonoBehaviour
+{
+    public bool Selected { get; set; }
+    public bool flag = false;
+
+}
